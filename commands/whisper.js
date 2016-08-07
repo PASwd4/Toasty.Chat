5c1126ed983e8c49916d95fc1fcab84c3569d580
ToastyStoemp <foreverwolf@live.be>
2016-08-07 20:58:07 +0200

    added fix for nicknames starting with @

--- commands/whisper.js
+++ commands/whisper.js
@@ -1,6 +1,7 @@
 var whipser = function(bot, sender, args, data, client)
 {
   var targetNick = args.splice(0,1);
+  targetNick = targetNick.replace("@", "");
   var friend = bot.chatServerBase.getClientOfChannel(targetNick, client.channel) || null;
   if (friend != null) {
     bot.sendClient(sender + " whispers: " + args.join(' '), friend);
